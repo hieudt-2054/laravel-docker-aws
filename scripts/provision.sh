47b0cc40fc5c7bad37534c2942c6428a4ade6e83
hieudt-2054 <55786352+hieudt-2054@users.noreply.github.com>
2024-02-03 19:50:07 +0000

    update provision

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,8 +1,8 @@
+sudo add-apt-repository ppa:ondrej/php
 apt update
 
-apt install --no-install-recommends php8.1
 
-apt-get install -y php8.1-fpm php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+sudo apt-get install -y php8.2-cli php8.2-common php8.2-fpm php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath
 
 apt install nodejs
 
